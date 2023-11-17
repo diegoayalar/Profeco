eee34246d19b7930ebc1e3b6d69b600339d546d2
Diego Ayala <diego.ayala228359@potros.itson.edu.mx>
2023-11-16 22:53:58 -0700

    Agregar rutas

---
+++ Servidor/app.js
@@ -0,0 +1,28 @@
+const dotenv = require("dotenv");
+const express = require("express");
+const cors = require("cors");
+const conectarDB = require('./db');
+const entidadesRutas = require("./rutas/entidadesRutas.js");
+
+dotenv.config({ path: "./config.env" });
+
+const app = express();
+
+conectarDB();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Rutas API
+app.use("/api", entidadesRutas);
+
+app.get("/", (req, res) => {
+    res.send("Hello from the backend!");
+});
+
+// Puerto en el que escucha el servidor
+const puerto = process.env.PORT;
+app.listen(puerto, () => {
+    console.log(`Server is running on port ${puerto}`);
+});
