3f1e23172a909fdf0b81c75b3f4a87c2c98efe6a
Diego Ayala <diego.ayala228359@potros.itson.edu.mx>
2023-11-18 16:58:29 -0700

    Agregar componentes

---
+++ Cliente/src/components/LoginForm.vue
@@ -0,0 +1,62 @@
+<template>
+    <div class="container mt-5">
+      <div class="row justify-content-center">
+        <div class="col-md-6">
+          <div class="card">
+            <div class="card-header bg-primary text-white">
+              <h4 class="mb-0">Iniciar Sesión</h4>
+            </div>
+            <div class="card-body">
+              <form @submit.prevent="login">
+                <div class="form-group">
+                  <label for="email">Correo Electrónico:</label>
+                  <input
+                    type="email"
+                    class="form-control"
+                    id="email"
+                    v-model="email"
+                    placeholder="Ingrese su correo electrónico"
+                    required
+                  />
+                </div>
+                <div class="form-group">
+                  <label for="password">Contraseña:</label>
+                  <input
+                    type="password"
+                    class="form-control"
+                    id="password"
+                    v-model="password"
+                    placeholder="Ingrese su contraseña"
+                    required
+                  />
+                </div>
+                <br />
+                <button type="submit" class="btn btn-primary btn-block">
+                  Iniciar Sesión
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        email: "",
+        password: "",
+      };
+    },
+    methods: {
+      login() {
+        // Lógica para procesar el inicio de sesión
+        console.log("Correo Electrónico:", this.email);
+        console.log("Contraseña:", this.password);
+        // Aquí puedes realizar una solicitud HTTP para autenticar al usuario
+      },
+    },
+  };
+  </script>
