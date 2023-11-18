89f055c134a4666602c894756988f6b6e304e074
Diego Ayala <diego.ayala228359@potros.itson.edu.mx>
2023-11-18 16:58:45 -0700

    Agregar rutas

---
+++ Cliente/src/router/index.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Inicio from '../views/Inicio.vue'
+import Productos from '../views/Productos.vue'
+import FormularioProducto from '../views/FormularioProducto.vue'
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      component: Inicio
+    },
+    {
+      path: '/productos',
+      component: Productos
+    },
+    {
+      path: '/productos/nuevo',
+      component: FormularioProducto
+    },
+  ],
+})
+
+export default router
