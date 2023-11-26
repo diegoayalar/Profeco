230bc07d9932aced45f4ba3b3756240ef0405afe
Diego Ayala <diego.ayala228359@potros.itson.edu.mx>
2023-11-25 20:14:30 -0700

    Agregar validaciones y mensajes de advertencia

--- Servidor/modelos/usuario.js
+++ Servidor/modelos/usuario.js
@@ -1,15 +1,43 @@
+const bcrypt = require('bcrypt');
 const mongoose = require("mongoose");
+const validator = require('validator');
 
 const esquemaUsuario = new mongoose.Schema(
     {
-        nombre: { type: String, required: true, unique: true },
-        correoElectronico: { type: String, required: true, unique: true },
-        contrasena: { type: String, required: true },
+        nombre: { 
+            type: String, 
+            required: [true, 'Por favor, proporciona un nombre.'],
+            unique: [true, 'Este nombre ya está en uso.']
+        },
+        correoElectronico: { 
+            type: String, 
+            required: [true, 'Por favor, proporciona una dirección de correo electrónico.'], 
+            unique: [true, 'Esta dirección de correo electrónico ya está registrada.'],
+            validate: {
+                validator: function (value) {
+                    return validator.isEmail(value);
+                },
+                message: 'Por favor, introduce una dirección de correo electrónico válida.'
+            }
+        },
+        contrasena: {
+            type: String,
+            required: [true, 'Por favor, proporciona una contraseña.'],
+            validate: {
+                validator: function (value) {
+                    return value.length >= 8;
+                },
+                message: 'La contraseña debe tener al menos 8 caracteres.'
+            }
+        },
         rol: {
             type: String,
-            enum: ["Consumidor", "Mercado", "Profeco"],
+            enum: {
+                values: ["Consumidor", "Mercado", "Profeco"],
+                message: 'El rol debe ser uno de: Consumidor, Mercado, Profeco.'
+            },
             default: "Consumidor",
-            required: true,
+            required: [true, 'Por favor, proporciona un rol.'],
         },
         fechaCreacion: { type: Date, default: Date.now },
     },
@@ -17,6 +45,16 @@ const esquemaUsuario = new mongoose.Schema(
         versionKey: false,
     }
 );
+
+esquemaUsuario.pre('save', async function (next) {
+    const usuario = this;
+    if (!usuario.isModified('contrasena')) return next();
+
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(usuario.contrasena, salt);
+    usuario.contrasena = hash;
+    next();
+});
 
 const usuario = mongoose.model("Usuario", esquemaUsuario);
 
