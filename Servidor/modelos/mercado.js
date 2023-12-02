7f426e6e428c72305d2c579439cb89d8ed144504
Diego Ayala <diego.ayala228359@potros.itson.edu.mx>
2023-12-01 22:28:04 -0700

    Agrega usuario y productos

--- Servidor/modelos/mercado.js
+++ Servidor/modelos/mercado.js
@@ -2,19 +2,34 @@ const mongoose = require("mongoose");
 
 const esquemaMercado = new mongoose.Schema(
     {
-        usuario: {
+        usuarioId: {
             type: mongoose.Schema.Types.ObjectId, ref: "Usuario",
             required: true,
         },
-        productos: [{ type: mongoose.Schema.Types.ObjectId, ref: "Producto" }],
+        productosIds: [{ type: mongoose.Schema.Types.ObjectId, ref: "Producto" }],
         notificaciones: [
             { type: mongoose.Schema.Types.ObjectId, ref: "Notificacion" },
         ],
     },
     {
         versionKey: false,
+        toJSON: { virtuals: true },
+        toObject: { virtuals: true },
     }
 );
+
+esquemaMercado.virtual("usuario", {
+    ref: "Usuario",
+    localField: "usuarioId",
+    foreignField: "_id",
+    justOne: true,
+  });
+
+  esquemaMercado.virtual("productos", {
+    ref: "Producto",
+    localField: "productosIds",
+    foreignField: "_id",
+  });
 
 const mercado = mongoose.model("Mercado", esquemaMercado);
 
